3681847e916e5eb35435f51fcfb71ecea11c552a
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-03-23 03:38:07 +0000

    Abstraction

--- OOPstudy/Main3.java
+++ OOPstudy/Main3.java
@@ -2,8 +2,13 @@ package OOPstudy;
 
 public class Main3 {
     public static void main(String[] args) {
-        car1 c1 = new car1();
-        c1.go();
+       Circle circle = new Circle(7);
+       Triangle triangle = new Triangle(6, 15);
+       Rectangle rectangle = new Rectangle(10, 8);
+       System.out.println("Area: " + circle.area());
+       System.out.println("Area: " + rectangle.area());
+       System.out.println("Area: " + triangle.area());
+
     } 
 
 }
---
+++ OOPstudy/Rectangle.java
@@ -0,0 +1,15 @@
+package OOPstudy;
+
+public class Rectangle extends Shape{
+    double length;
+    double width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+    @Override
+    double area() {
+        return length * width;
+    };
+}
