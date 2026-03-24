b6d29e29b837a05a7ab2538c22adbab933df8968
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-03-24 16:55:18 +0000

    Inheritance

--- OOPstudy/Main.java
+++ OOPstudy/Main.java
@@ -2,8 +2,9 @@ package OOPstudy;
 
 public class Main {
     public static void main(String[] args) {
-        Course c1 = new Course("MATH223", "Calculus 2", 98);
-        c1.getRemark();
-        System.out.println("Course code: " + c1.getCourseCode());
+       Manager m1 = new Manager("Joseph ", 8999, 90);
+       System.out.println("Pay: $" + m1.calculatePay());
+       Intern i1 = new Intern("Joseph", 0, 9, 900);
+       System.out.println("Pay: $" + i1.calculatePay());
     }
 }
--- OOPstudy/Manager.java
+++ OOPstudy/Manager.java
@@ -1,5 +1,13 @@
 package OOPstudy;
 
 public class Manager extends Worker{
-    
+    double bonus;
+    Manager(String name, double baseSalary, double bonus) {
+        super(name, baseSalary);
+        this.bonus = bonus;
+    }
+    @Override
+    public double calculatePay() {
+        return (baseSalary + bonus);
+    }
 }
