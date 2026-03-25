d54c77db3fe22c4b812ce0e4c81407678df60704
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-03-25 23:40:20 +0000

    Todays update

--- OOPstudy/Main.java
+++ OOPstudy/Main.java
@@ -2,9 +2,9 @@ package OOPstudy;
 
 public class Main {
     public static void main(String[] args) {
-       Manager m1 = new Manager("Joseph ", 8999, 90);
+       Manager m1 = new Manager("Joseph ", 800, 90);
        System.out.println("Pay: $" + m1.calculatePay());
-       Intern i1 = new Intern("Joseph", 0, 9, 900);
+       Intern i1 = new Intern("Joseph", 0, 9, 800);
        System.out.println("Pay: $" + i1.calculatePay());
     }
 }
