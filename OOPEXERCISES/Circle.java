d82b8b25f7ff084884c943309a8f3f6b87757ae2
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-03-26 00:08:29 +0000

    Refactor Circle class to use double for radius and update area calculation to use Math.PI; add Playable interface

--- OOPEXERCISES/Circle.java
+++ OOPEXERCISES/Circle.java
@@ -3,12 +3,12 @@ package OOPEXERCISES;
 public class Circle extends Shape{
     double radius;
 
-    Circle(float radius) {
+    Circle(double radius) {
         this.radius = radius;
     }
 
     @Override
     double calculateArea() {
-        return 22/7 * radius * radius;
+        return Math.PI * radius * radius;
     }
 }
