75b7d48be0a5e478e6ad689e9f7e667fbd4c7294
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-03-27 02:28:12 +0000

    Encapsulation

--- OOPEXERCISES/Main.java
+++ OOPEXERCISES/Main.java
@@ -2,15 +2,8 @@ package OOPEXERCISES;
 
 public class Main {
     public static void main(String[] args) {
-        Circle c1 = new Circle(9);
-
-        System.out.println("Area of Circle: " + c1.calculateArea());
-        System.out.println("Radius: " + c1.radius);
-        Rectangle r1 = new Rectangle(9, 10);
-        System.out.println("Area of Rectangle: " + r1.calculateArea());
-        System.out.println("Length " + r1.length);
-        System.out.println("Width" + r1.width);
-        Football f1 = new Football("Soccer");
-        f1.play();
+        Account ac1 = new Account("Joseph", 90);
+        ac1.deposit(90);
+        ac1.getBalance();
     }
 }
