69a98054e5b2ef1393f87a58bc0a9046645ba852
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-03-27 02:58:45 +0000

    Remove type field and constructor from Vehicle class

--- OOPEXERCISES/Vehicle.java
+++ OOPEXERCISES/Vehicle.java
@@ -1,10 +1,6 @@
 package OOPEXERCISES;
 
 public class Vehicle {
-    String type;
-    Vehicle(String type) {
-        this.type = type;
-    }
     void start() {
         System.out.println("Your vehicle engine" + type + "has started");
     }
