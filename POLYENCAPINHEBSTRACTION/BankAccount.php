bc24dce8bd8b836e76b3be56bf1251dbfb99d237
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-03-27 04:27:37 +0000

    PHP OOP

---
+++ POLYENCAPINHEBSTRACTION/BankAccount.php
@@ -0,0 +1,42 @@
+<?php
+
+
+class BankAccount {
+    private $balance;
+
+    public function __construct($balance) {
+        $this->balance = $balance;
+    }
+
+    public function deposit($amount) {
+        if ($amount > 0) {
+            $this->balance += $amount;
+            echo "You have recieved $$amount";
+        } else {
+            echo "Invalid deposit amount";
+        }
+    }
+
+    public function withdraw($amount) {
+        if($amount > 0 && $amount < $this->balance) {
+            $this->balance -= $amount;
+            echo "You have withrawn $$amount" . "<br>";
+        } else {
+            echo "Insufficient funds / Invalid withdrawal amount";
+        }
+    }
+
+    public function getBalance() {
+        return $this->balance;
+    }
+}
+
+require_once "BankAccount.php";
+
+$b1 = new BankAccount(0);
+
+$b1->deposit(500);
+
+$b1->withdraw(200);
+
+echo "Balance: ".$b1->getBalance();
