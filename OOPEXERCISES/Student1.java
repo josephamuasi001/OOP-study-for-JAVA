5213cedb76de5d4daa0be1ef8fdb21daec9cc19b
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-03-27 02:57:36 +0000

    Add Person, Student1, and Teacher classes with inheritance structure

---
+++ OOPEXERCISES/Student1.java
@@ -0,0 +1,9 @@
+package OOPEXERCISES;
+
+public class Student1 extends Person{
+    String studentId;
+    Student1(String name, int age, String studentId) {
+        super(name, age);
+        this.studentId = studentId;
+    }
+}
