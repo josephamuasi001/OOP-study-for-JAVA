28507de43d84b507dfb7cbf3152a95e9deab0351
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-03-30 23:13:51 +0000

    Placeholder

--- E-commerce.php
+++ E-commerce.php
@@ -437,14 +437,3 @@ class Database {
     private $conn;
     
     public function connect() {
-        try {
-            $this->conn = new PDO(
-                'mysql:host=' . $this->host . ';dbname=' . $this->db_name,
-                $this->user,
-                $this->password
-            );
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $this->conn;
-        } catch (PDOException $e) {
-            echo 'Connection Error: ' . $e->getMessage();
-            return null;
