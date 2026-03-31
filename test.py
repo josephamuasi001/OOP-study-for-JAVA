1b57cef17fc0bd26958456f0b0b365af00c4e8ee
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-03-31 05:14:16 +0000

    Add new python file

---
+++ test.py
@@ -0,0 +1,22 @@
+#Even/Odd number Investigation
+
+while True:
+    print("EVEN/ODD NUMBER SIMULATOR")
+    print("1.Check number parity")
+    print("2.Exit")
+
+    choice = int(input("Select an option : "))
+
+    if choice == 1:
+        number = int(input("Enter a number: "))
+
+        if number % 2 == 0 :
+            print("Even number")
+        else:
+            print("Odd Number")
+    elif choice == 2:
+        print("Goodbyeeee!!!!")
+        break
+
+    else:
+        print("Invalid option")
